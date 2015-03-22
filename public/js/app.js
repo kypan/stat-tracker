27afd3b4f84666e7d0311cdcc7fa34d4da832eb6
Kevin Pan <kevin.y.pan@gmail.com>
2015-03-22 01:24:43 -0700

    in progress 	-shot chart input fully functional 	-reset stats functional

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
 var Main = React.createClass({
 	loadStatsFromServer: function() {
 		$.ajax({
-			url: this.props.url,
+			url: '/stats',
 			dataType: 'json',
-			type: 'GET',
 			success: function(data) {
-				this.setState({data: data});
+				sortedPlayers = _.sortBy(data, function(player) {
+					return Number(player.number);
+				});
+				this.setState({data: sortedPlayers});
 			}.bind(this),
 			error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error('/stats', status, err.toString());
       }.bind(this)
 		});
 	},
@@ -19,49 +21,224 @@ var Main = React.createClass({
 		this.loadStatsFromServer();
     setInterval(this.loadStatsFromServer, this.props.pollInterval);
 	},
-	handleShotChartClick: function(coordinates) {
+	handleShotChartSubmit: function(FGAttempt) {
 		var players = this.state.data;
-		players[0].attemptedFG.push({x: coordinates.x, y: coordinates.y, made: true, assisted: false});
+		var i = _.findIndex(players, function(p) {
+			return p.number === FGAttempt.number;
+		});
+		players[i].attemptedFG.push(_.omit(FGAttempt, 'number'));
 		this.setState({data: players}, function() {
 			$.ajax({
-        url: this.props.url,
+        url: '/stats/recordShot',
         dataType: 'json',
-        type: 'PUT',
-        data: {x: coordinates.x, y: coordinates.y, made: true, assisted: false},
+        type: 'POST',
+        data: FGAttempt,
         success: function(data) {
           this.setState({data: data});
         }.bind(this),
         error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
+          console.error('/stats/shot', status, err.toString());
         }.bind(this)
       });
 		});
+	},
+	handleResetStats: function() {
+		var resetObject = {
+			attemptedFG: [],
+			attemptedFT: [],
+			rebounds: "0",
+			assists: "0",
+			steals: "0",
+			blocks: "0",
+			turnovers: "0",
+			fouls: "0"
+		};
+		var players = this.state.data;
+		_.forEach(players, function(player) {
+			_.assign(player, resetObject);
+		});
+		// this.setState({data: players}, function() {
+			$.ajax({
+				url: '/stats/reset',
+				dataType: 'json',
+				type: 'PUT',
+				success: function(data) {
+					this.setState({data: data});
+				}.bind(this),
+        error: function(xhr, status, err) {
+          console.error('/stats/reset', status, err.toString());
+        }.bind(this)
+			});
+		// });
 	},
 	render: function() {
 		return (
 			<div className="main">
 				<Clock />
 				<CurrentPlayers />
-				<ShotChart data={this.state.data} onShotChartClick={this.handleShotChartClick} />
+				<ShotChart data={this.state.data} onShotChartSubmit={this.handleShotChartSubmit} />
 				<OtherStats />
-				<BoxScore data={this.state.data} />
+				<BoxScore data={this.state.data} onResetStats={this.handleResetStats} />
 			</div>
 		);
 	}
 });
 
 var ShotChart = React.createClass({
+	player: null,
+	posX: null,
+	posY: null,
+	modalX: null,
+	modalY: null,
+	made: false,
+	getInitialState: function() {
+		return {showShooterModal: false, showAssisterModal: false};
+	},
 	handleClick: function(e) {
 		e.preventDefault();
-		var posX = e.pageX - $(".shot-chart").offset().left;
-		var posY = e.pageY - $(".shot-chart").offset().top;
-		alert(posX + ',' + posY);
-		this.props.onShotChartClick({x: posX, y: posY});
+		if(this.state.showShooterModal || this.state.showAssisterModal)
+			return;
+		this.posX = e.pageX - $(".shot-chart").offset().left - 337;
+		this.posY = -(e.pageY - $(".shot-chart").offset().top);
+		this.modalX = e.pageX;
+		this.modalY = e.pageY;
+		this.setState({showShooterModal: true});
+	},
+	handleShooterSubmit: function(data) {
+		if(!data.made) {
+			this.props.onShotChartSubmit({number: data.player, x: this.posX, y: this.posY, made: data.made});
+			this.reset();
+		}
+		else {
+			this.player = data.player;
+			this.made = data.made;
+			this.setState({showShooterModal: false, showAssisterModal: true});
+		}
+	},
+	handleAssisterSubmit: function(data) {
+		this.props.onShotChartSubmit({number: this.player, x: this.posX, y: this.posY, made: this.made, assistedBy: data.assister});
+		this.reset();
+	},
+	reset: function() {
+		this.posX = null;
+		this.posY = null;
+		modalX: null;
+		modalY: null;
+		this.player = null;
+		this.made = false;
+		this.setState({showShooterModal: false, showAssisterModal: false});
 	},
 	render: function() {
 		return (
 			<div className="shot-chart" onClick={this.handleClick}>
 				<img src="/img/shot_chart.png" />
+				{this.state.showShooterModal ?
+				 <ShotChartShooterModal data={this.props.data}
+				 												modalX={this.modalX}
+				 												modalY={this.modalY}
+				 												onShooterSubmit={this.handleShooterSubmit} /> : null}
+				{this.state.showAssisterModal ?
+				 <ShotChartAssisterModal data={this.props.data}
+				 												 modalX={this.modalX}
+				 												 modalY={this.modalY}
+				 												 shooter={this.player}
+				 												 onAssisterSubmit={this.handleAssisterSubmit} /> : null}
+			</div>
+		);
+	}
+});
+
+var ShotChartShooterModal = React.createClass({
+	getInitialState: function() {
+		return {showShooterOptions: true};
+	},
+	componentDidMount: function() {
+		$(".shot-chart-modal").css({
+			left: this.props.modalX,
+			top: this.props.modalY
+		});
+	},
+	handleSelectPlayerFocus: function(e) {
+		e.preventDefault();
+		this.setState({showShooterOptions: true});
+		shooterDropdown = document.getElementById("shooter-options");
+		_.forEach(this.props.data, function(player) {
+			shooterDropdown.options.add(new Option('#'+player.number+' '+player.name, player.number));
+		});
+	},
+	handleMadeClick: function(e) {
+		e.preventDefault();
+		var shooter = this.refs.shooterSelect.getDOMNode().value;
+		this.props.onShooterSubmit({player: shooter, made: true});
+		this.refs.shooterSelect.getDOMNode().value = '';
+	},
+	handleMissedClick: function(e) {
+		e.preventDefault();
+		var shooter = this.refs.shooterSelect.getDOMNode().value;
+		this.props.onShooterSubmit({player: shooter, made: false});
+		this.refs.shooterSelect.getDOMNode().value = '';
+	},
+	render: function() {
+		return (
+			<div>
+				<div className="modal-bg modal-bg-clear"></div>
+				<div className="shot-chart-modal">
+					<span>Who took the shot?</span>
+					<select id="shooter-options" onFocus={this.handleSelectPlayerFocus} ref="shooterSelect">
+						<option value="" disabled selected>Select player...</option>
+					</select>
+					<button className="made-btn" onClick={this.handleMadeClick}>MADE</button>
+					<button className="missed-btn" onClick={this.handleMissedClick}>MISSED</button>
+				</div>
+			</div>
+		);
+	}
+});
+
+var ShotChartAssisterModal = React.createClass({
+	getInitialState: function() {
+		return {showAssisterOptions: false};
+	},
+	componentDidMount: function() {
+		$(".shot-chart-modal").css({
+			left: this.props.modalX,
+			top: this.props.modalY
+		});
+	},
+	handleSelectAssisterFocus: function(e) {
+		e.preventDefault();
+		this.setState({showAssisterOptions: true});
+		var _this = this;
+		var assisterOptions = _.filter(this.props.data, function(player) {
+			return player.number !== _this.props.shooter;
+		});
+		assisterDropdown = document.getElementById("assister-options");
+		_.forEach(assisterOptions, function(assister) {
+			assisterDropdown.options.add(new Option('#'+assister.number+' '+assister.name, assister.number));
+		});
+	},
+	handleDoneClick: function(e) {
+		e.preventDefault();
+		var assister = this.refs.assisterSelect.getDOMNode().value;
+		this.props.onAssisterSubmit({assister: assister});
+		this.refs.assisterSelect.getDOMNode().value='';
+	},
+	handleNoAssistClick: function(e) {
+		e.preventDefault();
+		this.props.onAssisterSubmit({assister: null});
+	},
+	render: function() {
+		return (
+			<div>
+				<div className="modal-bg modal-bg-clear"></div>
+				<div className="shot-chart-modal">
+					<span>Who Assisted?</span>
+					<select id="assister-options" onFocus={this.handleSelectAssisterFocus} ref="assisterSelect">
+						<option value="" disabled selected>Select player...</option>
+					</select>
+					<button className="done-btn" onClick={this.handleDoneClick}>DONE</button>
+					<button className="done-btn" onClick={this.handleNoAssistClick}>No Assist</button>
+				</div>
 			</div>
 		);
 	}
@@ -98,22 +275,25 @@ var CurrentPlayers = React.createClass({
 });
 
 var BoxScore = React.createClass({
+	resetStats: function() {
+		this.props.onResetStats();
+	},
 	render: function() {
 		var rows = this.props.data.map(function(player, index) {
 			var attemptedFG = player.attemptedFG.length;
 			var madeFG = _.filter(player.attemptedFG, function(fg) {
-				return fg.made === true;
+				return fg.made === "true";
 			}).length;
 			var attemptedThreesList = _.filter(player.attemptedFG, function(fg) {
-				return (Math.pow(fg.x, 2) + Math.pow(fg.y, 2)) > 90000;
+				return (Math.pow(Number(fg.x), 2) + Math.pow(Number(fg.y), 2)) > 90000;
 			})
 			var attemptedThrees = attemptedThreesList.length;
 			var madeThrees = _.filter(attemptedThreesList, function(fg) {
-				return fg.made === true;
+				return fg.made === "true";
 			}).length;
 			var attemptedFT = player.attemptedFT.length;
 			var madeFT = _.filter(player.attemptedFT, function(ft) {
-				return ft.made === true;
+				return ft.made === "true";
 			}).length;
 			var points = (madeFG - madeThrees) * 2 + madeThrees * 3 + madeFT;
 			return (
@@ -134,30 +314,33 @@ var BoxScore = React.createClass({
 			);
 		});
 		return (
-			<div className="box-score">
-				<table className="box-score-table">
-					<tr className="box-score-header">
-						<th>#</th>
-						<th>Name</th>
-						<th>FG</th>
-						<th>3PT</th>
-						<th>FT</th>
-						<th>REB</th>
-						<th>AST</th>
-						<th>STL</th>
-						<th>BLK</th>
-						<th>TO</th>
-						<th>PF</th>
-						<th>PTS</th>
-					</tr>
-					{rows}
-				</table>
+			<div>
+				<div className="box-score">
+					<table className="box-score-table">
+						<tr className="box-score-header">
+							<th>#</th>
+							<th>Name</th>
+							<th>FG</th>
+							<th>3PT</th>
+							<th>FT</th>
+							<th>REB</th>
+							<th>AST</th>
+							<th>STL</th>
+							<th>BLK</th>
+							<th>TO</th>
+							<th>PF</th>
+							<th>PTS</th>
+						</tr>
+						{rows}
+					</table>
+				</div>
+				<button onClick={this.resetStats}>RESET STATS</button>
 			</div>
 		);
 	}
 });
 
 React.render(
-	<Main url="stats.json" pollInterval={200} />,
+	<Main pollInterval={200} />,
 	document.getElementById('stat-tracker')
 );
--- server.js
+++ server.js
@@ -9,22 +9,27 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/stats.json', function(req, res) {
+app.get('/stats', function(req, res) {
   fs.readFile('stats.json', function(err, data) {
     res.setHeader('Content-Type', 'application/json');
-    var stats = JSON.parse(data);
-    res.send(stats);
+    res.send(data);
   });
 });
 
-app.put('/stats.json', function(req, res) {
+app.put('/stats/reset', function(req, res) {
   fs.readFile('stats.json', function(err, data) {
-    var toAdd = _.pick(req.body, ['x', 'y', 'made', 'assisted']);
     var stats = JSON.parse(data);
-    var i = _.findIndex(stats, function(player) {
-      return player.number = req.body.number;
+     _.forEach(stats, function(player) {
+      player.attemptedFG = [];
+      player.attemptedFT = [];
+      player.rebounds = "0";
+      player.assists = "0";
+      player.steals = "0";
+      player.blocks = "0";
+      player.turnovers = "0";
+      player.fouls = "0";
     });
-    stats[0].attemptedFG.push(toAdd); //change index later
+    console.log(stats);
     fs.writeFile('stats.json', JSON.stringify(stats, null, 4), function(err) {
       res.setHeader('Content-Type', 'application/json');
       res.setHeader('Cache-Control', 'no-cache');
@@ -33,10 +38,19 @@ app.put('/stats.json', function(req, res) {
   });
 });
 
-app.post('/stats.json', function(req, res) {
+app.post('/stats/recordShot', function(req, res) {
   fs.readFile('stats.json', function(err, data) {
     var stats = JSON.parse(data);
-    stats.push(req.body);
+    var i = _.findIndex(stats, function(player) {
+      return player.number === req.body.number;
+    });
+    stats[i].attemptedFG.push(_.omit(req.body, 'number'));
+    if(req.body.assistedBy) {
+      var j = _.findIndex(stats, function(player) {
+        return player.number === req.body.assistedBy;
+      });
+      stats[j].assists = Number(stats[j].assists) + 1;
+    }
     fs.writeFile('stats.json', JSON.stringify(stats, null, 4), function(err) {
       res.setHeader('Content-Type', 'application/json');
       res.setHeader('Cache-Control', 'no-cache');
